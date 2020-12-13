d9b58ca78abd96efed9a522ed72a25db3a6a08da
Reda LAHRACHE <reda.lahrache@outlook.com>
2020-12-13 18:59:52 +0100

    l'ajout du Header partie 1/2

---
+++ Header.php
@@ -0,0 +1,87 @@
+ <div class="Header">
+ <!--Barre1 de navigation-->
+ <nav class="min-navbar">
+    <ul class="elts-min-navbar">
+        <li><a><i class="fas fa-phone-alt"></i> +212 6 xx xx xx</a></li>
+        <li><a href="#">A propos</a></li>
+    </ul>
+</nav>
+
+ <!--Barre2 de navigation-->
+ <nav class="menu-nav">
+    <div class="wrapper">
+      <div class="logo"><a href="#">CodingNepal</a></div>
+      <input type="radio" name="slider" id="menu-btn">
+      <input type="radio" name="slider" id="close-btn">
+      <ul class="nav-links">
+        <label for="close-btn" class="btn close-btn"><i class="fas fa-times"></i></label>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li>
+          <a href="#" class="desktop-item">Dropdown Menu</a>
+          <input type="checkbox" id="showDrop">
+          <label for="showDrop" class="mobile-item">Dropdown Menu</label>
+          <ul class="drop-menu">
+            <li><a href="#">Drop menu 1</a></li>
+            <li><a href="#">Drop menu 2</a></li>
+            <li><a href="#">Drop menu 3</a></li>
+            <li><a href="#">Drop menu 4</a></li>
+          </ul>
+        </li>
+        <li>
+          <a href="#" class="desktop-item">Mega Menu</a>
+          <input type="checkbox" id="showMega">
+          <label for="showMega" class="mobile-item">Mega Menu</label>
+          <div class="mega-box">
+            <div class="content">
+              <div class="row">
+                <header>Design Services</header>
+                <ul class="mega-links">
+                  <li><a href="#">Graphics</a></li>
+                  <li><a href="#">Vectors</a></li>
+                  <li><a href="#">Business cards</a></li>
+                  <li><a href="#">Custom logo</a></li>
+                </ul>
+              </div>
+              <div class="row">
+                <header>Email Services</header>
+                <ul class="mega-links">
+                  <li><a href="#">Personal Email</a></li>
+                  <li><a href="#">Business Email</a></li>
+                  <li><a href="#">Mobile Email</a></li>
+                  <li><a href="#">Web Marketing</a></li>
+                </ul>
+              </div>
+              <div class="row">
+                <header>Security services</header>
+                <ul class="mega-links">
+                  <li><a href="#">Site Seal</a></li>
+                  <li><a href="#">VPS Hosting</a></li>
+                  <li><a href="#">Privacy Seal</a></li>
+                  <li><a href="#">Website design</a></li>
+                </ul>
+            </div>
+          </div>
+        </li>
+        <li><a href="#">Feedback</a></li>
+      </ul>
+      <ul class="user-tools">
+        <li class="nav-links"><a href="#" style="color: white;"><i class="far fa-user"></i></a></li>
+        <li class="nav-links"><a href="#" style="color: white;"><i class="fas fa-shopping-cart"></i></a></li>
+        <li class="nav-links"><a href="#" style="color: white;"><i class="far fa-heart"></i></a></li>
+        <li>
+          <div class="search-box">
+            <form action="" method="POST">
+              <input class="search-txt" style="display:inline;" type="text" placeholder="Chercher des produits">
+              <a class="search-btn" href="#"> <i class="fas fa-search"></i></a> 
+            </form>
+          </div>
+        </li>
+      </ul>
+      <label for="menu-btn" class="btn menu-btn"><i class="fas fa-bars"></i></label>
+    </div>
+  </nav>
+</div>
+
+
+<br><br><br><br><br><br><br><br><br><br><br><br>
--- Login.php
+++ Login.php
@@ -7,12 +7,17 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <!-- Style CSS -->
     <link href="LoginStyle.css" type="text/css" rel="stylesheet">
+    <link href="HeaderStyle.css" type="text/css" rel="stylesheet">
     <!-- Font -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Crimson+Text&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 </head>
 <body>
-    
+  <!--Header-->
+  <?php include "Header.php" ?>
+
+  <!--Inscription - Connexion-->
     <div class="container-fluid">
     	<div class="row Login_Row justify-content-center">
     		<div class="col-md-4 col-xs-12" id="Block_connexion">
@@ -50,6 +55,5 @@
             </div>
     	</div>
     </div>
-
 </body>
 </html>
