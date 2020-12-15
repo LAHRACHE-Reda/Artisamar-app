0f2525c9949776023d004c0b3aac1a8c01f05597
Asma-HAOUA <haouaasmae61@gmail.com>
2020-12-15 20:29:34 +0100

    sf hadi khedama

--- Achat.php
+++ Achat.php
@@ -11,9 +11,12 @@
 	<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Crimson+Text&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 
 	<!-- add style -->
-	<link rel="AchatStyle.css" type="text/css" href="comStyle.css"> </head>
+	<link rel="stylesheet" type="text/css" href="AchatStyle.css"> </head>
     <link href="HeaderStyle.css" type="text/css" rel="stylesheet">
 <body>
   <!--Header-->
